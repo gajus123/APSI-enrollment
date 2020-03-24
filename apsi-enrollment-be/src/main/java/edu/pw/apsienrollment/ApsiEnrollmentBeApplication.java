3638aac1285eb625b45179ed836d2c0c5b77cf2a
Delebrith <paulina.szwed.1996@gmail.com>
2020-03-24 13:42:00 +0100

    Application stub

---
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/ApsiEnrollmentBeApplication.java
@@ -0,0 +1,13 @@
+package edu.pw.apsienrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsiEnrollmentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApsiEnrollmentBeApplication.class, args);
+	}
+
+}
---
+++ apsi-enrollment-be/src/test/java/edu/pw/apsienrollment/ApsiEnrollmentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pw.apsienrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsiEnrollmentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
