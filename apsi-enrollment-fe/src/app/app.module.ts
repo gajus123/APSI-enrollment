2cb7cd80524544726f09884a0b3467caa10b1f97
Wojciech Gruszka <wojciech@gruszka.eu>
2020-03-25 13:32:18 +0100

    Conversion to SCSS, application stub

--- apsi-enrollment-fe/src/app/app.module.ts
+++ apsi-enrollment-fe/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { LoginModule } from './modules/login/login.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, LoginModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
