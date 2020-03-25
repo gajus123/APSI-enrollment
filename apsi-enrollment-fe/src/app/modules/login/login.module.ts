d7b3be41045fe3bd9d03a75e3a38a46eb4ad388e
Wojciech Gruszka <wojciech@gruszka.eu>
2020-03-25 18:53:20 +0100

    Login page

--- apsi-enrollment-fe/src/app/modules/login/login.module.ts
+++ apsi-enrollment-fe/src/app/modules/login/login.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginRoutingModule } from './login.routing';
 import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [CommonModule, LoginRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class LoginModule {}
--- apsi-enrollment-fe/src/app/modules/login/pages/login/login.component.ts
+++ apsi-enrollment-fe/src/app/modules/login/pages/login/login.component.ts
@@ -1,11 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { timer } from 'rxjs';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  loginForm: FormGroup;
+  isError: boolean;
+  isLoading: boolean;
 
-  ngOnInit(): void {}
+  constructor(public fb: FormBuilder) {
+    this.loginForm = fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+
+    this.isError = false;
+    this.isLoading = false;
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.isLoading) {
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.valid) {
+      this.tryAuthenticate(username, password);
+    }
+  }
+
+  tryAuthenticate(login: string, password: string) {
+    this.isLoading = true;
+    this.loginForm.disable();
+    // TODO: use auth service from TG-14 instead of timer
+    timer(1000).subscribe(() => {
+      this.isLoading = false;
+      this.loginForm.enable();
+    });
+  }
 }
