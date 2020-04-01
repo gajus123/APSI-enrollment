0ad5aa93ba17fb0598651a43ae96b987531d6faf
Delebrith <paulina.szwed.1996@gmail.com>
2020-04-01 23:27:33 +0200

    add possibility to revoke refresh token after one use. Change form of refresh token and refresh endpoint

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/authentication/api/AuthenticationController.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/authentication/api/AuthenticationController.java
@@ -1,11 +1,11 @@
 package edu.pw.apsienrollment.authentication.api;
 
-import edu.pw.apsienrollment.authentication.AuthTokenType;
 import edu.pw.apsienrollment.authentication.AuthenticationService;
-import edu.pw.apsienrollment.authentication.api.dto.AccessTokenDto;
 import edu.pw.apsienrollment.authentication.api.dto.AuthenticatedUserDto;
 import edu.pw.apsienrollment.authentication.api.dto.CredentialsDto;
+import edu.pw.apsienrollment.authentication.api.dto.RefreshTokenDto;
 import edu.pw.apsienrollment.authentication.api.dto.TokenDto;
+import edu.pw.apsienrollment.user.db.User;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -22,26 +22,29 @@ public class AuthenticationController {
 
     @ApiOperation(value = "Authenticate", nickname = "login", notes = "")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "If valid credentials were provided", response = AccessTokenDto.class),
+            @ApiResponse(code = 200, message = "If valid credentials were provided", response = RefreshTokenDto.class),
             @ApiResponse(code = 400, message = "If invalid data was provided")})
     @PostMapping("authenticate")
     ResponseEntity<TokenDto> authenticate(@RequestBody CredentialsDto registrationDto) {
-        String authenticated = authenticationService.authenticate(registrationDto.getUsername(), registrationDto.getPassword());
+        User authenticated = authenticationService.authenticate(registrationDto.getUsername(), registrationDto.getPassword());
         return ResponseEntity.ok(TokenDto.builder()
-                .accessToken(authenticationService.getToken(authenticated, AuthTokenType.ACCESS_TOKEN))
-                .refreshToken(authenticationService.getToken(authenticated, AuthTokenType.REFRESH_TOKEN))
+                .accessToken(authenticationService.getAccessToken(authenticated))
+                .refreshToken(authenticationService.getRefreshToken(authenticated))
                 .build());
     }
 
     @ApiOperation(value = "Refresh token", nickname = "refresh", notes = "")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "If valid credentials were provided", response = AccessTokenDto.class),
+            @ApiResponse(code = 200, message = "If valid credentials were provided", response = RefreshTokenDto.class),
             @ApiResponse(code = 400, message = "If invalid data was provided")})
-    @GetMapping("refresh")
-    ResponseEntity<AccessTokenDto> refreshAuthentication(@RequestParam String refreshToken) {
-        String authenticated = authenticationService.authenticateFromRefreshToken(refreshToken);
-        return ResponseEntity.ok(new AccessTokenDto(
-                authenticationService.getToken(authenticated, AuthTokenType.ACCESS_TOKEN)));
+    @PostMapping("refresh")
+    ResponseEntity<TokenDto> refreshAuthentication(@RequestBody RefreshTokenDto refreshTokenDto) {
+        User authenticated = authenticationService.authenticateFromRefreshToken(refreshTokenDto.getRefreshToken());
+        authenticationService.invalidateRefreshToken(refreshTokenDto.getRefreshToken());
+        return ResponseEntity.ok(TokenDto.builder()
+                .accessToken(authenticationService.getAccessToken(authenticated))
+                .refreshToken(authenticationService.getRefreshToken(authenticated))
+                .build());
     }
 
     @ApiOperation(value = "Get authenticated user info", nickname = "get user info", notes = "",
